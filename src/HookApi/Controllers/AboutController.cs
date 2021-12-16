b9223491e7f44fb9c05ecc28c464e8dfa8bb4935
a.asadi <ali64b@gmail.com>
2021-12-16 16:36:19 +0330

    update about controller

--- src/HookApi/Controllers/AboutController.cs
+++ src/HookApi/Controllers/AboutController.cs
@@ -22,7 +22,7 @@ namespace HookApi.Controllers
 		[HttpGet]
 		public ActionResult Get()
 		{
-			return new JsonResult(new { MyNameIs = "Ali2009" });
+			return new JsonResult(new { MyNameIs = "Ali2021" });
 		}
 	}
 }
