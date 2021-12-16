e674586033a70ae032995e8472b3b2945a0de4fd
a.asadi <ali64b@gmail.com>
2021-12-16 12:56:33 +0330

    update git

--- bash-ci.sh
+++ bash-ci.sh
@@ -1,12 +1,12 @@
 logFile=("./git-log-file-$(date '+%Y-%m-%d-%H-%M-%S').log")
 color="\e[1;35m"
-script logFile
+script $logFile
 
 # container name
 $WORKER_SERVICE_NAME = '{container id}'
 
 #  checkout branch to feature/pre-commit #
 echo -e $color"checkout branch to feature/pre-commit"
-(git checkout feature/pre-commit -f)
+#(git checkout feature/pre-commit -f)
 
-exitasd
+exit
