75de6fa3a184dfae25bc043f385c4feb0c91fc3d
a.asadi <ali64b@gmail.com>
2021-12-16 14:43:00 +0330

    update files

--- bash-ci.sh
+++ bash-ci.sh
@@ -1,12 +1,48 @@
-logFile=("./git-log-file-$(date '+%Y-%m-%d-%H-%M-%S').log")
-color="\e[1;35m"
-script $logFile
+#! /bin/bash
 
+#logsave -a log.txt  ./bash-ci.sh
+
+logFile="./git-log-file-$(date '+%Y-%m-%d-%H-%M-%S').log";
+
+
+echo $logFile
 # container name
-$WORKER_SERVICE_NAME = '{container id}'
+WORKER_SERVICE_NAME = "{container id}"
 
 #  checkout branch to feature/pre-commit #
 echo -e $color"checkout branch to feature/pre-commit"
-#(git checkout feature/pre-commit -f)
+git checkout feature/pre-commit -f
 
-exit
+# removes staged and working directory changes in the server
+git reset --hard
+
+#  pull feature/pre-commit branch #
+echo "pull feature/pre-commit branch"
+git pull
+
+currentLastCommitHash="$(git rev-parse HEAD)"
+echo "latest hash code:$currentLastCommitHash"
+
+# read latest commit hash code from last-commit.txt file
+lastCommitHash="$(cat ./last-commit.txt)"
+
+# compare the current commit hash with previous the commit hash
+isUpdate =  [[  $currentLastCommitHash == $lastCommitHash ]] && echo true || echo false
+
+# pre-commit branch is updated?
+echo "is updated: $isUpdate"
+
+
+if [ isUpdate ]
+then
+
+docker-compose config
+docker-compose build --no-cache
+docker-compose up -d --force-recreate
+
+
+# update last-commit.txt file
+echo $currentLastCommitHash > ./last-commit.txt
+write-host "publish finished"
+
+fi
